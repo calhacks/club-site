27737989ee9600374f8ad178f3f97e705ee4d428
Kenny Zhang <122967787+kennyzhang0819@users.noreply.github.com>
2025-06-06 15:59:02 -0700

    spons page fix

--- components/sponsor/SponsorHero.tsx
+++ components/sponsor/SponsorHero.tsx
@@ -11,12 +11,12 @@ export function SponsorHero() {
   return (
     <div className="relative overflow-hidden">
 
-      <div className="max-w-7xl mx-auto px-6 pt-24 pb-16 md:pt-32 md:pb-24 relative z-0">
+      <div className="max-w-7xl mx-auto px-6 pt-24 pb-8 md:pt-32 md:pb-12 relative z-0">
         <h1 className="text-5xl md:text-6xl font-bold font-heading mb-12 text-gray-900 dark:text-white">
           Make Cal Hacks your event.
         </h1>
 
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8">
+        <div className="flex flex-row space-x-8">
           <div className="bg-white/80 dark:bg-gray-800/80 backdrop-blur-sm p-6 rounded-xl">
             <h4 className="text-xl font-bold text-primary mb-2">Establish your brand!</h4>
             <p className="text-gray-700 dark:text-gray-300">
@@ -46,14 +46,22 @@ export function SponsorHero() {
 
         </div>
 
-        <div className="p-6 border-none rounded-xl">
-          <h4 className="text-xl font-bold text-primary mb-2">
+        <div className="p-6 flex flex-col gap-2">
+          <h4 className="flex flex-row gap-2 text-xl font-bold text-primary">
+            Our next event is in
+            <div className="text-blue-600 dark:text-blue-400 hover:underline">
+            October 2025!
+            </div>
+            
+          </h4>
+
+          <h4 className="text-xl font-bold text-primary">
             Ready to jump onboard? Email{' '}
             <a
-              href="mailto:sponsorship@hackberkeley.org"
+              href="mailto:team@calhacks.io"
               className="text-blue-600 dark:text-blue-400 hover:underline"
             >
-              sponsorship@hackberkeley.org
+              team@calhacks.io
             </a>
           </h4>
         </div>
