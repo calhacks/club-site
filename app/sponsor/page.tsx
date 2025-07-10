83886858bb025243cce91d197f5562b91a0e44d8
Sam Poder <sam.r.poder@gmail.com>
2025-07-10 18:05:55 -0500

    Bring context us above past + testimonials

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -17,9 +17,9 @@ export default function Sponsor() {
       <main>
         <SponsorHero />
         <SponsorStats />
+        <SponsorContact />
         <SponsorPast />
         <SponsorTestimonials />
-        <SponsorContact />
       </main>
     </DefaultLayout>
   );
