37f3d24dbf9f89558373ca98ac3c515fee660e3d
Sam Poder <sam.r.poder@gmail.com>
2025-07-10 18:23:48 -0500

    Add AI logos

--- components/sponsor/SponsorPast.tsx
+++ components/sponsor/SponsorPast.tsx
@@ -1,21 +1,38 @@
 import Image from 'next/image'
+import Logos from "@/assets/images/ai-spons-logos.png";
 
 export function SponsorPast() {
   return (
-    <section className="py-16">
+    <>
+      <section className="py-16">
         <h2 className="text-4xl font-bold font-heading text-center mb-16 text-gray-900 dark:text-white">
-          11.0 Sponsors
+          AI Hackathon 2025 Sponsors
         </h2>
         
         <div className="flex justify-center invert dark:invert-0">
           <Image 
-            src="/vectors/sponsors.svg"
+            src={Logos}
             alt="Past sponsors grid"
-            width={1000}
             height={600}
             className="w-full max-w-4xl"
           />
         </div>
-    </section>
+      </section>
+      <section className="py-16">
+          <h2 className="text-4xl font-bold font-heading text-center mb-16 text-gray-900 dark:text-white">
+            11.0 Sponsors
+          </h2>
+          
+          <div className="flex justify-center invert dark:invert-0">
+            <Image 
+              src="/vectors/sponsors.svg"
+              alt="Past sponsors grid"
+              width={1000}
+              height={600}
+              className="w-full max-w-4xl"
+            />
+          </div>
+      </section>
+    </>
   )
 } 
