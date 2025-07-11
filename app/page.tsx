489aa15b6f4324e0f1213a7d6c21ed8a2852472a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2025-07-10 20:10:44 -0400

    MLH

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,6 @@ export default function Home() {
   return (
     <DefaultLayout>
       <main>
-        <a
-          id="mlh-trust-badge"
-          style={{
-            display: 'block',
-            maxWidth: '100px',
-            minWidth: '60px',
-            position: 'fixed',
-            right: '50px',
-            top: '0',
-            width: '10%',
-            zIndex: 10000
-          }}
-          href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=blue"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-blue.svg"
-            alt="Major League Hacking 2026 Hackathon Season"
-            style={{ width: '100%' }}
-          />
-        </a>
         <Hero />
         <Initiatives />
         <PhotoGallery />
