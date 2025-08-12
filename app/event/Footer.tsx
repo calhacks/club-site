0f43f684299b7b700d895c87ceb6306ed4d7dfa9
Sam Poder <sam.r.poder@gmail.com>
2025-08-11 21:27:51 -0500

    Update Footer.tsx

--- app/event/Footer.tsx
+++ app/event/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
 	return (
-		<footer className="absolute bottom-0 flex h-fit w-full justify-center py-6 sm:py-8 md:py-10">
+		<footer className="absolute bottom-0 flex h-fit w-full justify-center py-6 sm:py-8 md:py-10 px-4 sm:px-6 md:px-8 ">
 			<span className="px-4 sm:px-6 md:px-8 text-center font-neue text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg font-normal text-white leading-relaxed">
-				Interested in sponsoring us? Reach out at{" "}
+				Interested in joining Anthropic & Windsurf and sponsoring Cal Hacks? Reach out at{" "}
 				<a
 					href="mailto:sponsorship@hackberkeley.org"
 					className="underline decoration-white decoration-1 sm:decoration-2 underline-offset-2 hover:opacity-80 transition-opacity"
